90843c5fe36659b7160a75b18601dc13eadf1e42
Mirzohid2226 <smm2226fbus@gmail.com>
2020-09-30 22:49:59 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll',function(){
+			var header = document.querySelector('header');
+			header.classList.toggle('sticy',window.scrollY > 0);
+		});
+function toggleMenu(){
+	var menuTogle = document.querySelector('.toggle');
+	var menu = document.querySelector('.menu');
+	menuTogle.classList.toggle('active');
+	menu.classList.toggle('active');
+}
+function clickToggle(){
+	var menuSubmit = document.querySelector('.button');
+	menuSubmit.classList.button('tooll');
+}
